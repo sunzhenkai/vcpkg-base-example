c89c971271df23f7c0131483f9ebc3649ee17dfd
zhenkai.sun <zhenkai.sun@qq.com>
2024-05-16 00:45:51 +0800

    fix datastax-cpp-driver

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Example for [vcpkg-base](https://github.com/sunzhenkai/vcpkg-base).
 cmake --preset=default
 cmake --build vcpkg-build
 ```
+
+# Overlay Port
+```shell
+vcpkg install {port} --overlay-ports=/path/to/source/overlay-port
+
+# for example
+vcpkg install datastax-cpp-driver --overlay-ports=/home/wii/code/vcpkg-base-example/overlay-port
+```
---
+++ overlay-port/datastax-cpp-driver/portfile.cmake
@@ -0,0 +1,28 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_git(
+        OUT_SOURCE_PATH SOURCE_PATH
+        URL https://github.com/datastax/cpp-driver.git
+        REF "e05897d72fdac08a212ed3136b7790232670e329"
+        HEAD_REF master
+        PATCHES cmake.patch
+)
+
+message(STATUS "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT_PACKAGES_DIR}")
+
+vcpkg_cmake_configure(
+        SOURCE_PATH ${SOURCE_PATH}
+        OPTIONS
+        -DCASS_BUILD_SHARED=OFF
+        -DCASS_BUILD_STATIC=ON
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_pkgconfig()
+# copy from ${CONFIG_PATH} to share/${PACKAGE_NAME}
+vcpkg_cmake_config_fixup(PACKAGE_NAME "${PORT}" CONFIG_PATH "share/cassandra_static")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
