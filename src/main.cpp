e6d245113d810b689a3d56bd46ec73e5d0673528
zhenkai.sun <zhenkai.sun@qq.com>
2024-05-22 00:36:16 +0800

    gperftools

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "babylon/anyflow/builder.h"
+#include "gperftools/tcmalloc.h"
+#include "leveldb/db.h"
 
 int main() {
     std::cout << "hello, vcpkg." << std::endl;
+    auto r = tc_malloc(100);
+    tc_free(r);
     return 0;
 }
