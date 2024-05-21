cc75204a481e38dc58df2577b136cb8035dd5037
zhenkai.sun <zhenkai.sun@qq.com>
2024-05-22 00:51:43 +0800

    add ppconsul

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include "babylon/anyflow/builder.h"
 #include "gperftools/tcmalloc.h"
 #include "leveldb/db.h"
+#include "ppconsul/ppconsul.h"
 
 int main() {
     std::cout << "hello, vcpkg." << std::endl;
     auto r = tc_malloc(100);
     tc_free(r);
+
+    ppconsul::kv::kw::keys;
     return 0;
 }
