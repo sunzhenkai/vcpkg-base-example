8c147134f9c308e897df9d13f5bc698d3a5c781c
Wii <zhenkai.sun@qq.com>
2024-06-14 15:21:15 +0800

    Wip opentel (#3)
    add opentelemetry-cpp
    add polaris-cpp

---
+++ overlay-port/polaris-cpp/portfile.cmake
@@ -0,0 +1,38 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_git(
+        OUT_SOURCE_PATH SOURCE_PATH
+        URL https://github.com/polarismesh/polaris-cpp.git
+        REF "ea0f459c8d71b9c64d6eef3626bb93a41efc962a"
+        HEAD_REF main
+)
+
+message(STATUS "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT_PACKAGES_DIR}")
+
+vcpkg_execute_build_process(
+    COMMAND make -j
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME "build_polaris")
+
+vcpkg_execute_build_process(
+    COMMAND sh "${CMAKE_CURRENT_LIST_DIR}/re_archive.sh"
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME "archive_polaris")
+
+# add_custom_target(combined ALL
+#    COMMAND ${CMAKE_CXX_ARCHIVE_CREATE} libcombined.a $<TARGET_FILE:lib1> $<TARGET_FILE:lib2>)
+
+# for debug
+#execute_process(COMMAND mkdir -p ${SOURCE_PATH}/build64/lib COMMAND touch ${SOURCE_PATH}/build64/lib/libpolaris_api.a)
+
+# debug
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
+file(COPY ${SOURCE_PATH}/libpolaris_api.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+# release
+file(COPY ${SOURCE_PATH}/libpolaris_api.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(COPY ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
